547e5ab356ec597c6dd34508c553c6322a6d93a5
owenyi <67588446+owenyi@users.noreply.github.com>
2021-07-25 17:29:41 +0900

    update tic-tac-toe

---
+++ 05_tic-tac-toe/TableComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <table>
+      <tr-component v-for="(rowData, rowIndex) in tableData" :row-data="rowData" :row-index="rowIndex" :key="rowIndex"></tr-component>
+  </table>
+</template>
+
+<script>
+  import TrComponent from './TrComponent'
+;
+  export default {
+    components: {
+      TrComponent,
+    },
+    props: {
+      tableData: Array,
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 05_tic-tac-toe/TrComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr>
+	  <td-component v-for="(cellData, cellIndex) in rowData" :cell-data="cellData" :cell-index="cellIndex" :row-index="rowIndex" :key="cellIndex"></td-component>
+  </tr>
+</template>
+
+<script>
+  import TdComponent from './TdComponent';
+
+  export default {
+    components: {
+      TdComponent,
+    },
+    props: {
+      rowData: Array,
+      rowIndex: Number,
+    },
+    data() {
+      return {
+        parentData: '내가 니 애비다.'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
