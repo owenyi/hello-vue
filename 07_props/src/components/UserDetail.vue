703fe48ab5534afafb66beb2a3d3675fea387310
owenyi <67588446+owenyi@users.noreply.github.com>
2021-07-28 16:02:53 +0900

    update cli props

---
+++ 07_props/src/components/UserDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @clcik="switchName">이름 변경</v-btn>
+  </div>
+</template>
+
+<script>
+export default ({
+  props: {
+    nameOfChild: {
+      type: String,
+      default: 'Lego',
+      required: true,
+    },
+  },
+  data() {
+    return {
+      // name: '뷰제이에스',
+    }
+  },
+  computed: {
+    sayHello() {
+      return this.nameOfChild + ' 안녕하세요.'
+    }
+  },
+  methods: {
+    switchName() {
+      this.nameOfChild = '컴퓨터'
+    }
+  }
+})
+</script>
