8ae503b08e88d339c8624ed0269af268bdfafaa7
owenyi <67588446+owenyi@users.noreply.github.com>
2021-08-03 15:31:01 +0900

    update router

---
+++ 12_router/src/views/Users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <h2>params</h2>
+    <p>이 유저는 현재 유저 번호가 {{ userId }}입니다.</p>
+    <p>{{ $route.params.name }}</p>
+    <h2>query</h2>
+    <p>그룹 : {{ $route.query.group }}</p>
+    <p>카테고리 : {{ $route.query.category }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      userId() {
+        return this.$route.params.userId
+      }
+    },
+    created() {
+      console.log("router", this.$router)
+      console.log("route", this.$route)
+    }
+  }
+</script>
+
+<style>
+
+</style>
